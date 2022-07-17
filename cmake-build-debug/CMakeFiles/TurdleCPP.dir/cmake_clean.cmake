811d61a48819528d6b30b5c6dd54d022a8236ec9
Jacob Howard <jah0147@auburn.edu>
2022-07-17 17:01:01 -0500

    sound files added (not functioning)

---
+++ cmake-build-debug/CMakeFiles/TurdleCPP.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TurdleCPP.dir/console.cpp.obj"
+  "CMakeFiles/TurdleCPP.dir/console.cpp.obj.d"
+  "CMakeFiles/TurdleCPP.dir/delay.cpp.obj"
+  "CMakeFiles/TurdleCPP.dir/delay.cpp.obj.d"
+  "CMakeFiles/TurdleCPP.dir/game.cpp.obj"
+  "CMakeFiles/TurdleCPP.dir/game.cpp.obj.d"
+  "CMakeFiles/TurdleCPP.dir/main.cpp.obj"
+  "CMakeFiles/TurdleCPP.dir/main.cpp.obj.d"
+  "CMakeFiles/TurdleCPP.dir/wordGen.cpp.obj"
+  "CMakeFiles/TurdleCPP.dir/wordGen.cpp.obj.d"
+  "TurdleCPP.exe"
+  "TurdleCPP.exe.manifest"
+  "TurdleCPP.pdb"
+  "libTurdleCPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TurdleCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
